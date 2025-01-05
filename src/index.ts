efee1e66d9af49c5269f2c97cf0fc310cb1240c2
001Rakib <rakibrakibrakib354@gmail.com>
2025-01-05 19:53:20 +0600

    complented initial backend setup

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import * as dynamoose from "dynamoose";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// ROUTE IMPORTS
+
+// CONFIGURATION
+dotenv.config();
+const isProduction = process.env.NODE_ENV === "production";
+
+if (isProduction) {
+  dynamoose.aws.ddb.local();
+}
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
